bb1d650c3dbbb40a25cd59549ecd7a0abc3c84b7
Yaprak <yaprekinkaya@gmail.com>
2024-01-18 13:24:00 +0300

    EnemyPatrol
    :P

--- Assets/Scripts/MonsterDamage.cs
+++ Assets/Scripts/MonsterDamage.cs
@@ -10,7 +10,7 @@ public class MonsterDamage : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player"){
-            playerHealth.TakeDamage(damage);
+            playerHealth.TakeDamage(damage);   
         }
     }
 }
